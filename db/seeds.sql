39f80bd128ce9c45cb15b25e6254c43901c4e21a
Abbey Hudson <a_hudson2@yahoo.com>
2017-09-28 13:58:34 -0700

    working app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Santa Fe Burger');
+INSERT INTO burgers (burger_name) VALUES ('New Orleans Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');

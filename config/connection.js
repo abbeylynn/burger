6f9ddb57c80fb7f46dac673871a7c6a8bce5db6d
Abbey Hudson <a_hudson2@yahoo.com>
2017-09-28 14:09:08 -0700

    updated connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,22 @@ var connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "cookie",
-	database: "burgers_db"
+	database: "burgers_db",
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "cookie",
+		database: "burgers_db"
+	});
+}
+}
 });
+
+
 
 connection.connect(function(err) {
 	if(err) {

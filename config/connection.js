4c094ad5db5362e97d1c002e1b34e11dbf95bb5d
Abbey Hudson <a_hudson2@yahoo.com>
2017-09-28 17:53:35 -0700

    updated connection code

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "cookie",
-	database: "burgers_db",
+var connection;// = mysql.createConnection({
+	//port: 3306,
+	//host: "localhost",
+	//user: "root",
+	//password: "cookie",
+	//database: "burgers_db",
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -18,7 +18,7 @@ if (process.env.JAWSDB_URL) {
 	});
 }
 }
-});
+//});
 
 
 
